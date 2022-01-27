feef84890a737008125f91633b00608a1b26e9c4
dEN5 <54153548+dEN5-tech@users.noreply.github.com>
2022-01-27 22:45:54 +0300

    Update parameters.py

--- n/parameters.py
+++ n/parameters.py
@@ -4,13 +4,13 @@ DEBUG = False
 START_EPOCH = 0
 
 EXPLORE_PROB = .2
-POOL_SIZE = 64
+POOL_SIZE = int(os.environ.get('POOL_SIZE', None))
 VIEW_RATIO = .2
 ATTITUDE_TOWARDS_NOVEL_TAGS = 1
 
-JOB_POOL_SIZE = 8
-JSON_MAX = 16
-PORT = os.environ.get('PORT', None)
+JOB_POOL_SIZE = int(os.environ.get('JOB_POOL_SIZE', None))
+JSON_MAX = int(os.environ.get('JSON_MAX', None))
+PORT = int(os.environ.get('PORT', None))
 
 FILTER = os.environ.get('FILTER', None)
 
